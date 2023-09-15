302e8ba0a3e19e96858863300b0755450a091c45
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-09-15 06:49:09 +0600

    added price and credit icon and fix the height of the cards

--- course-registration/src/components/Course.jsx
+++ course-registration/src/components/Course.jsx
@@ -1,9 +1,9 @@
-import { FaBeer } from 'react-icons/fa';
+import { FaBookOpen,FaDollarSign } from 'react-icons/fa';
 const Course = ({course,handleCourseName}) => {
     const{course_img,course_title,course_description,price,credit} = course
     return (
         <div>
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card w-[312px] h-[402px] bg-base-100 shadow-xl">
   <figure className="px-10 pt-10">
     <img src={course_img} alt="" className="rounded-xl" />
   </figure>
@@ -11,8 +11,15 @@ const Course = ({course,handleCourseName}) => {
     <h2 className="card-title">{course_title}</h2>
     <p>{course_description}</p>
     <div className="flex gap-3">
-        <p>$ Price = {price}</p>
-        <p> Credit: {credit}hr</p>
+      <div className='flex justify-center items-center'>
+        <FaDollarSign></FaDollarSign>
+        <p>Price = {price}</p>
+        </div>
+        <div className='flex gap-2 justify-center items-center'>
+        <p><FaBookOpen></FaBookOpen></p>
+        <p><span>Credit: {credit}hr</span> </p>
+        </div>
+
         </div>
 
     <div className="card-actions">
