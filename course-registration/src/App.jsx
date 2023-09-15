b82c56408aa776f1af4d984e3a12b9e62c099966
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-09-15 07:56:11 +0600

    added toast and couse name

--- course-registration/src/App.jsx
+++ course-registration/src/App.jsx
@@ -4,13 +4,23 @@ import './App.css'
 import CourseDetail from './CourseDetail'
 import Header from './components/Header'
 import Courses from './components/courses'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
-  const [courseName , setCourseName] = useState([])
-  const handleCourseName = course=>{
-    const courseNameAdded = [...courseName, course]
-   setCourseName(courseNameAdded)
-  }
+  const [courseName, setCourseName] = useState([]);
+  const [selectedCourse, setSelectedCourse] = useState([]);
+
+  const handleCourseName = (course) => {
+    const isSelected = selectedCourse.find((name) => name.id === course.id);
+
+    if (isSelected) {
+      toast.error('No duplicates allowed');
+    } else {
+      setCourseName([...courseName, course]);
+      setSelectedCourse([...selectedCourse, course]);
+    }
+  };
   return (
     <>
       <Header></Header>
@@ -18,6 +28,7 @@ function App() {
       <Courses handleCourseName={handleCourseName}></Courses>
       <CourseDetail courseName ={courseName}></CourseDetail>
       </div>
+      <ToastContainer position='top-left' autoClose={1000}></ToastContainer>
     </>
   )
 }
