6e4a6086f05ece054faa5f06b359d6d5e0f3c52d
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-09-15 09:47:36 +0600

    fixed dark theme to light andadd course name section into a card

--- course-registration/src/CourseDetail.jsx
+++ course-registration/src/CourseDetail.jsx
@@ -3,14 +3,22 @@ import CourseName from "./CourseName";
 const CourseDetail = ({courseName,totalCredit,totalHourRemains}) => {
     return (
         <div className="">
-            <h2 className="text-[#2F80ED] my-5">Credit Hour Remaining {totalHourRemains}</h2>
+            <div className="card w-96 bg-base-100 shadow-xl">
+  <div className="card-body">
+  <h2 className="text-[#2F80ED] my-5">Credit Hour Remaining {totalHourRemains}</h2>
             <hr />
-            <h3 className="font-bold my-5">Course Name  </h3>
+            <h3 className="font-extrabold text-2xl my-5">Course Name  </h3>
             {
                 courseName.map( listOfcourse=><CourseName listOfcourse={listOfcourse}></CourseName>)
             }
             <hr className="my-5" />
             <h3 className="my-10">Total Credit Hour : {totalCredit}</h3>
+    <div className="card-actions justify-end">
+
+    </div>
+  </div>
+</div>
+
         </div>
     );
 };
